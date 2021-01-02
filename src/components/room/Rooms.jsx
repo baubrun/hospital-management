28282c3bf37122e68b493ced5c17f2cbc6689c67
baubrun <baubelf@gmail.com>
2021-01-01 19:22:45 -0500

    added rooms

---
+++ src/components/room/Rooms.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Paper from "@material-ui/core/Paper";
+
+
+const
+
+const Rooms = () => {
+    
+
+    return (
+        <Paper>
+            <svg>
+                <g>
+                    <text></text>
+                    <circle r={5}></circle>
+                </g>
+            </svg>
+        </Paper>
+    )
+}
+
+
+export default Rooms
