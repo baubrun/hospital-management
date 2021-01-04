4b8616ec129f398a50f7b80c8210a5e062a91d4b
baubrun <baubelf@gmail.com>
2021-01-03 20:57:49 -0500

    updated patient container

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/styles";
 import { theme } from "./css/theme";
+import NavBar from "./components/NavBar"
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 
 import PatientFormContainer from "./components/patient/patientForm/PatientFormContainer";
 import NotFound from "./views/NotFound";
 import Rooms from "./components/rooms/Rooms"
+import PatientContainer from "./components/patient/PatientContainer"
 
-import NavBar from "./components/NavBar"
-import CssBaseline from '@material-ui/core/CssBaseline';
 
 const App = () => {
 
@@ -21,6 +23,7 @@ const App = () => {
         <Switch>
         <Route exact={true} path="/" component={PatientFormContainer} />
         <Route exact={true} path="/rooms" component={Rooms}/>
+        <Route exact={true} path="/patients" component={PatientContainer}/>
         <Route component={NotFound} />
         </Switch>
       </ThemeProvider>
--- src/components/navMenu/navItems.js
+++ src/components/navMenu/navItems.js
@@ -2,16 +2,23 @@ import React from "react";
 
 import GridOnIcon from "@material-ui/icons/GridOn";
 import QueueIcon from "@material-ui/icons/Queue";
+import AccessibilityNewIcon from '@material-ui/icons/AccessibilityNew';
 
 export const navItems = [
+  {
+    name: "New Patient",
+    icon: <QueueIcon />,
+    url: "/",
+  },
+
   {
     name: "Occupancy",
     icon: <GridOnIcon />,
     url: "/rooms",
   },
   {
-    name: "New Patient",
-    icon: <QueueIcon />,
-    url: "/",
+    name: "Patients",
+    icon: <AccessibilityNewIcon />,
+    url: "/patients",
   },
 ];
